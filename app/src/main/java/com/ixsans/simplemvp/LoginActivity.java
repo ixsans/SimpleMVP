b4be234c5e49437ccf3a790dc5a55abcb733813a
Ikhsanudin Hakim <ikhsanudinhakim@gmail.com>
2016-10-06 10:38:02 +0700

    add oncreatemenu options

--- app/src/main/java/com/ixsans/simplemvp/LoginActivity.java
+++ app/src/main/java/com/ixsans/simplemvp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
@@ -82,5 +83,9 @@ public class LoginActivity extends AppCompatActivity{
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 }
 
